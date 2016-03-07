03b123b675e69d8a140296a3a2bb3f289abf8d04
minhdtb83 <minhdtb@atmarkcafe.org>
2016-03-07 09:03:57 +0700

    optimize source

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,8 +1,6 @@
 import com.minhdtb.storm.Main;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
