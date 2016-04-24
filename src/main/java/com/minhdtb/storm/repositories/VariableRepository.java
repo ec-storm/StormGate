8465d742c7bfad4d8884da5d503a3c0fa014689d
minhdtb <minhdtb.83@gmail.com>
2016-04-24 10:46:03 +0700

    update coding

--- src/main/java/com/minhdtb/storm/repositories/VariableRepository.java
+++ src/main/java/com/minhdtb/storm/repositories/VariableRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface VariableRepository extends CrudRepository<Variable, Long>, JpaRepository<Variable, Long> {
-    
+
     @Modifying(clearAutomatically = true)
     void delete(Variable variable);
 }
